52c8e9c556528325cc1ad392ccd3e9259ab01b52
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2023-01-03 13:55:56 +1300

    Add files via upload

---
+++ 01_convertor_gui_v1.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+
+class Converter:
+
+    def __init__(self):
+
+        # Set up GUI Frame
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor",
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and " \
+                       "then press one of the buttons to convert " \
+                       "it from centigrade to Fahrenheit."
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wrap=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
