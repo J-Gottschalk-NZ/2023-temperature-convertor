a29faa863254ee2eaefbf117fdacb5fb67ab72c0
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2023-01-08 14:20:42 +1300

    Add files via upload

---
+++ 06_help_GUI_v1.py
@@ -0,0 +1,92 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+
+class Converter:
+
+    def __init__(self):
+        # common format for all buttons
+        # Arial size 14 bold, with white text
+        button_font = ("Arial", "12", "bold")
+        button_fg = "#FFFFFF"
+
+        # Set up GUI Frame
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.button_frame = Frame(padx=30, pady=30)
+        self.button_frame.grid(row=0)
+
+        self.to_help_button = Button(self.button_frame,
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     fg=button_fg,
+                                     font=button_font, width=12,
+                                     command=self.to_help)
+        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
+
+    def to_help(self):
+        DisplayHelp(self)
+
+
+class DisplayHelp:
+
+    def __init__(self, partner):
+
+        # setup dialogue box and background colour
+        background = "#ffe6cc"
+        self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # If users press cross at top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help,
+                                                           partner))
+
+        self.help_frame = Frame(self.help_box, width=300, height=200,
+                                bg=background)
+        self.help_frame.grid()
+
+        self.help_heading_label = Label(self.help_frame, bg=background,
+                                        text="Help / Info",
+                                        font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0)
+
+        help_text = "To use the program, simply enter the temperature " \
+                    "you wish to convert and then choose to convert " \
+                    "to either degrees Celsius (centigrade) or " \
+                    "Fahrenheit..  \n\n" \
+                    " Note that -273 degrees C " \
+                    "(-459 F) is absolute zero (the coldest possible " \
+                    "temperature).  If you try to convert a " \
+                    "temperature that is less than -273 degrees C, " \
+                    "you will get an error message. \n\n " \
+                    "To see your " \
+                    "calculation history and export it to a text " \
+                    "file, please click the 'History / Export' button."
+        self.help_text_label = Label(self.help_frame, bg=background,
+                                     text=help_text, wrap=350,
+                                     justify="left")
+        self.help_text_label.grid(row=1, padx=10)
+
+        self.dismiss_button = Button(self.help_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help, partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+
+    # closes help dialogue (used by button and x at top of dialogue)
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
